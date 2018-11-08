33a07632680fdf60043d661db122cfc9ea591a43
Juan J. Perea <jjperea@intranet.proconsultingpartners.com>
2018-11-08 17:06:42 -0600

    first commit

---
+++ src/main/java/mx/com/cloud/Lab9GatewayApplication.java
@@ -0,0 +1,21 @@
+package mx.com.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class Lab9GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab9GatewayApplication.class, args);
+	}
+	
+	@Bean
+    public ShallowEtagHeaderFilter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
+    }    
+}
